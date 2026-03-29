1862591f48642ad7cb1abdb2157c4d925b6c0b48
greyydahm-max <greyydahm@gmail.com>
2026-03-28 21:06:17 -0500

    Update TorosAutoAttackClient.java

--- src/client/java/pl/torobolin/autoattack/TorosAutoAttackClient.java
+++ src/client/java/pl/torobolin/autoattack/TorosAutoAttackClient.java
@@ -1,21 +1,46 @@
 package pl.torobolin.autoattack;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.*;
+import java.nio.file.*;
 
 public class TorosAutoAttackClient implements ClientModInitializer {
 
-    private static AutoAttackConfigModel config;
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("toros-auto-attack.json");
+    private static AutoAttackConfigModel config = new AutoAttackConfigModel();
 
     @Override
     public void onInitializeClient() {
-        AutoConfig.register(AutoAttackConfigModel.class, GsonConfigSerializer::new);
-        config = AutoConfig.getConfigHolder(AutoAttackConfigModel.class).getConfig();
-
+        loadConfig();
         AutoAttackHandler handler = new AutoAttackHandler();
         new KeybindsHandler().registerKeyInputs(handler);
         handler.handleAttack();
+    }
+
+    private static void loadConfig() {
+        if (Files.exists(CONFIG_PATH)) {
+            try (Reader reader = Files.newBufferedReader(CONFIG_PATH)) {
+                config = GSON.fromJson(reader, AutoAttackConfigModel.class);
+                if (config == null) config = new AutoAttackConfigModel();
+            } catch (IOException e) {
+                config = new AutoAttackConfigModel();
+            }
+        } else {
+            saveConfig();
+        }
+    }
+
+    public static void saveConfig() {
+        try (Writer writer = Files.newBufferedWriter(CONFIG_PATH)) {
+            GSON.toJson(config, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static AutoAttackConfigModel getConfig() {
