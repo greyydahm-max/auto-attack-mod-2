d9cd73a631b744a8602b773716b02cd2e17424d4
greyydahm-max <greyydahm@gmail.com>
2026-03-28 21:05:33 -0500

    Update ModMenuIntegration.java

--- src/client/java/pl/torobolin/autoattack/ModMenuIntegration.java
+++ src/client/java/pl/torobolin/autoattack/ModMenuIntegration.java
@@ -2,16 +2,14 @@ package pl.torobolin.autoattack;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.screens.Screen;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> (Screen) AutoConfig.getConfigScreen(AutoAttackConfigModel.class, parent);
+        return parent -> null;
     }
 }
