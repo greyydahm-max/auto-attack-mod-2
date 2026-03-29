314b81b414f7d4ee46edb7d39f6f258fb428fb26
greyydahm-max <greyydahm@gmail.com>
2026-03-28 21:07:04 -0500

    Update AutoAttackConfigModel.java

--- src/client/java/pl/torobolin/autoattack/AutoAttackConfigModel.java
+++ src/client/java/pl/torobolin/autoattack/AutoAttackConfigModel.java
@@ -1,33 +1,14 @@
 package pl.torobolin.autoattack;
 
-import me.shedaniel.autoconfig.ConfigData;
-import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
-
-@Config(name = "toros-auto-attack")
-public class AutoAttackConfigModel implements ConfigData {
-
+public class AutoAttackConfigModel {
     public boolean useCustomInterval = false;
-
     public int interval = 20;
-
     public boolean useRandomInterval = false;
-
-    @ConfigEntry.Gui.Tooltip(count = 0)
     public int minRandomInterval = 10;
-
-    @ConfigEntry.Gui.Tooltip(count = 0)
     public int maxRandomInterval = 40;
-
-    @ConfigEntry.Gui.Tooltip(count = 2)
     public boolean attackOnlyLivingEntities = true;
-
-    @ConfigEntry.Gui.Tooltip(count = 4)
     public AutoAttackType autoAttackType = AutoAttackType.ALL;
-
     public boolean sendToggleMessages = true;
-
     public boolean showMessagesInActionBar = true;
-
     public boolean attackPassiveEntities = false;
 }
